1383cce926bc9e75484e895b35c9d40c6965c169
VichitGit <v.vichitpov@gmail.com>
2017-08-12 23:55:45 +0700

    Callback DetailSingerSong

--- app/src/main/java/com/vichit/khmersong/adapter_layout/SingerCustomAdapter.java
+++ app/src/main/java/com/vichit/khmersong/adapter_layout/SingerCustomAdapter.java
@@ -1,87 +1,87 @@
-//package com.vichit.khmersong.adapter_layout;
-//
-//import android.content.Context;
-//import android.support.v7.widget.RecyclerView;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
-//
-//import com.makeramen.roundedimageview.RoundedImageView;
-//import com.squareup.picasso.Picasso;
-//import com.vichit.khmersong.R;
-//import com.vichit.khmersong.song_respone.SongRespones;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * Created by VichitDeveloper on 7/24/17.
-// */
-//
-//public class SingerCustomAdapter extends RecyclerView.Adapter<SingerCustomAdapter.SingerViewHolder> {
-//
-//    List<SongRespones> songList;
-//    SongRespones songRespones;
-//    Context context;
-//
-////    public SingerCustomAdapter(List<MusicModel> musicModelList, Context context) {
-////        this.musicModelList = musicModelList;
-////        this.context = context;
-////    }
-//
-//
-//    public SingerCustomAdapter(Context context) {
-//        songList = new ArrayList<>();
-//        this.context = context;
-//    }
-//
-//    public void addMoreSong(List<SongRespones> songList) {
-//        this.songList = songList;
-//    }
-//
-//    @Override
-//    public SingerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//
-//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-//        View view = inflater.inflate(R.layout.custom_layout_singer, parent, false);
-//
-//        return new SingerViewHolder(view);
-//
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(SingerViewHolder holder, int position) {
-//        songRespones = songList.get(position);
-//        String getNameSinger = songRespones.getSingerName();
-//        String getSinger = songRespones.getSingerImage();
-//
-//
-//
-//            holder.tvSinger.setText(songRespones.getSingerName());
-//        Picasso.with(context)
-//                .load(songRespones.getSingerImage())
-//                .into(holder.ivSinger);
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return songList.size();
-//    }
-//
-//
-//    class SingerViewHolder extends RecyclerView.ViewHolder {
-//        //ImageView ivSinger;
-//        RoundedImageView ivSinger;
-//        TextView tvSinger;
-//
-//        public SingerViewHolder(View itemView) {
-//            super(itemView);
-//            tvSinger = (TextView) itemView.findViewById(R.id.tvSingerName_Singer);
-//            ivSinger = (RoundedImageView) itemView.findViewById(R.id.ivSinger);
-//
-//
-//        }
-//    }
-//}
+package com.vichit.khmersong.adapter_layout;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
+import com.vichit.khmersong.R;
+import com.vichit.khmersong.callback.OnClickListener;
+import com.vichit.khmersong.song_respone.SingerResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SingerCustomAdapter extends RecyclerView.Adapter<SingerCustomAdapter.SingerViewHolder> {
+
+    List<SingerResponse.Singer> singersList;
+    SingerResponse.Singer singerRespones;
+    Context context;
+    OnClickListener onClickListener;
+
+
+    public SingerCustomAdapter(Context context) {
+        singersList = new ArrayList<>();
+        this.context = context;
+    }
+
+    public void addMoreSong(List<SingerResponse.Singer> singersList) {
+        this.singersList = singersList;
+    }
+
+    @Override
+    public SingerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.custom_layout_singer, parent, false);
+
+        return new SingerViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(SingerViewHolder holder, int position) {
+        singerRespones = singersList.get(position);
+
+
+        holder.tvSinger.setText(singerRespones.getSingerName());
+        Picasso.with(context)
+                .load(singerRespones.getSingerImage())
+                .into(holder.ivSinger);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return singersList.size();
+    }
+
+    public void setOnItemClick(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+
+    }
+
+
+    class SingerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        RoundedImageView ivSinger;
+        TextView tvSinger;
+
+        public SingerViewHolder(View itemView) {
+            super(itemView);
+            tvSinger = (TextView) itemView.findViewById(R.id.tvSingerName_Singer);
+            ivSinger = (RoundedImageView) itemView.findViewById(R.id.ivSinger);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onClickListener.onClickView(getAdapterPosition(), v);
+        }
+    }
+}
---
+++ app/src/main/java/com/vichit/khmersong/fragment/main/DetailSingerSongFragment.java
@@ -0,0 +1,170 @@
+package com.vichit.khmersong.fragment.main;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.vichit.khmersong.R;
+import com.vichit.khmersong.adapter_layout.MusicCustomAdapter;
+import com.vichit.khmersong.callback.OnClickListener;
+import com.vichit.khmersong.callback.OnPassData;
+import com.vichit.khmersong.interface_generator.SongService;
+import com.vichit.khmersong.service_generator.ServiceGenerator;
+import com.vichit.khmersong.song_respone.SongRespones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class DetailSingerSongFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, OnClickListener {
+    List<SongRespones.Songs> listSong;
+    SongRespones songRespones;
+    List<SongRespones.Songs> selectedSingersSongs;
+    SongService songService;
+    MusicCustomAdapter adapter;
+    RecyclerView rvSongBySinger;
+    SwipeRefreshLayout swipeRefreshSingerDetail;
+    OnPassData onPassData;
+    int getSingerId;
+    protected static String LOG = "ppppp";
+
+
+    public DetailSingerSongFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail_singer_song, container, false);
+
+        rvSongBySinger = (RecyclerView) view.findViewById(R.id.rvDetailSingerSong);
+        rvSongBySinger.setLayoutManager(new LinearLayoutManager(getContext(),
+                LinearLayoutManager.VERTICAL, false));
+
+        swipeRefreshSingerDetail = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshDetailSinger);
+        swipeRefreshSingerDetail.setOnRefreshListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle bundle = this.getArguments();
+        getSingerId = bundle.getInt("key");
+
+        adapter = new MusicCustomAdapter(getContext());
+        getAllSongBySinger();
+
+        adapter.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onRefresh() {
+        getAllSongBySinger();
+        swipeRefreshSingerDetail.setRefreshing(false);
+
+    }
+
+
+    private void getAllSongBySinger() {
+        songService = ServiceGenerator.createService(SongService.class);
+        Call<SongRespones> callAllSong = songService.findAllSongBySinger();
+        callAllSong.enqueue(new Callback<SongRespones>() {
+            @Override
+            public void onResponse(Call<SongRespones> call, Response<SongRespones> response) {
+                selectedSingersSongs = new ArrayList<>();
+                songRespones = response.body();
+                listSong = songRespones.getSongs();
+
+                for (SongRespones.Songs songs : listSong) {
+                    if (songs.getSinger().getId() == getSingerId) {
+                        selectedSingersSongs.add(songs);
+                    }
+                }
+                adapter.addMoreItem(selectedSingersSongs);
+                rvSongBySinger.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<SongRespones> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+
+
+    //click on menu
+    @Override
+    public void onClickView(int position, View view) {
+        PopupMenu popupMenu = new PopupMenu(getContext(), view);
+        popupMenu.inflate(R.menu.add_favorite);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.popup_Favorite:
+                        showMessage("Favorite");
+                        break;
+                    case R.id.popup_cencel:
+                        showMessage("Cancel");
+                        break;
+                }
+
+                return false;
+            }
+
+        });
+        popupMenu.show();
+
+
+    }
+
+    //reference with activity
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Activity activity;
+        if (context instanceof Activity) {
+            activity = (Activity) context;
+            onPassData = (OnPassData) activity;
+        }
+    }
+
+    //click item
+    @Override
+    public void onItemClick(List<SongRespones.Songs> songList, int postion) {
+        sendData(songList, postion);
+
+    }
+
+    public void sendData(List<SongRespones.Songs> songList, int postion) {
+        onPassData.onPassDataToActivity(songList, postion);
+
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+}
